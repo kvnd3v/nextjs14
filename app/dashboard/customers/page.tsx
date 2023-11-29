7178532b54f4df47d4ab42ade00f2730c104b7e4
Kevin <fwd.kevin@gmail.com>
2023-11-29 22:57:02 +0700

    Update : Customers Page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,26 @@
 import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: 'Customer'
 }
+
+export default function Page({ searchParams }: { searchParams?: { query?: string }; }) {
+    const query = searchParams?.query || '';
+
+    return (
+        <div className="w-full">
+            <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+                Customers
+            </h1>
+            <Search placeholder="Search customers..." />
+            <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+                <Table query={query} />
+            </Suspense>
+        </div>
+    );
 }
